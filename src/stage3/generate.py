be1754603d62fb595eb676055184f3ba3bc57351
Anciety <anciety@pku.edu.cn>
2019-12-03 16:47:07 +0800

    adjust word encoding pattern, to better suit the commit history of rime IME

--- src/stage3/generate.py
+++ src/stage3/generate.py
@@ -3,6 +3,7 @@ pypinyin heteronym words are fucked up, choose only the first one!
 '''
 import sys
 import copy
+import itertools
 import sqlite3
 from pypinyin import pinyin, Style
 from migrate import vocal_encode_pinyin
@@ -31,9 +32,6 @@ def nop(*args, **kwargs):
     pass
 print = nop
 
-MAX_LENGTH = 0
-
-
 def get_encodings_cache(conn):
     # encodings cache
     # word -> ([vocals], [shapes])
@@ -45,86 +43,97 @@ def get_encodings_cache(conn):
         vocals = row[1].split()
         shapes = row[2].split()
 
-        encodings[ch] = (vocals, shapes)
+        vocal_set = set()
+        for vocal in vocals:
+            vocal_set.add(vocal)
+
+        shape_set = set()
+        for shape in shapes:
+            shape_set.add(shape)
+
+        encodings[ch] = (vocal_set, shape_set)
 
     return encodings
 
 
-def get_heteronym_pairs(initials, finals):
-    initial_idx = 0
-    final_idx = 0
-    initial_len = len(initials)
-    final_len = len(finals)
-    heteronym_pairs = []
-    while initial_idx < initial_len or final_idx < final_len:
-        choose_initial = initial_idx if initial_idx < initial_len else initial_len - 1 
-        choose_final = final_idx if final_idx < final_len else final_len - 1
-        heteronym_pairs.append((initials[choose_initial], finals[choose_final]))
-        print('gen:')
-        print(heteronym_pairs, initial_idx, final_idx)
-        if initial_idx < len(initials):
-            initial_idx += 1
-        if final_idx < len(finals):
-            final_idx += 1
-    return heteronym_pairs
-
-        
 def gen_word_encoding(word, encodings):
-    global MAX_LENGTH
     for ch in word:
-        if encodings.get(ch) is None and ord(ch) > 0xff:
+        if encodings.get(ch) is None and (ord(ch) > 0xff or ch.isnumeric()):
             return []
+
     print('--- {} ---'.format(word))
-    initials = pinyin(word, style=Style.INITIALS, heteronym=True, strict=False)
-    finals = pinyin(word, style=Style.FINALS, heteronym=True, strict=False)
-    pairs = list(zip(initials, finals))
+    initials = pinyin(word, style=Style.INITIALS, strict=False)
+    finals = pinyin(word, style=Style.FINALS, strict=False)
+    pairs = []
+
+    eng = False
+
+    idx = 0
+
+    for ch in word:
+        if ord(ch) <= 0xff and eng:
+            print(pairs[-1][0])
+            pairs[-1][0] += ch
+            continue
+        elif ord(ch) <= 0xff:
+            eng = True
+        else:
+            eng = False
+        pairs.append([ch, (initials[idx], finals[idx])])
+
+        idx += 1
+
+    basic_encoding = [] # [(vocal_encoding, shape_encoding)]
+
+    encountered_non_hanzi = False
 
     print(pairs)
-    # XXX: pypinyin only has first usable !!
-    #counts = max(map(lambda x: max(len(x[0]), len(x[1])), pairs))
-    counts = 1
-    print(counts)
 
-    word_encoding = ['' for i in range(counts)]
+    for ch, (initial, final) in pairs:
+        # pypinyin is broken, only first prounounciation is quite correct
+        initial = initial[0]
+        final = final[0]
 
-    for initial, final in pairs:
         if initial == final:
-            # non hanzi XXX only first !!
-            word_encoding[0] += initial[0].lower()
-            continue
-        if counts > 1 and (len(initial) > 1 or len(final) > 1):
-            # heteronym
-            heteronym_pairs = get_heteronym_pairs(initial, final)
-        else:
-            heteronym_pairs = [(initial[0], final[0]) for i in range(counts)]
-
-        for i in range(counts):
-            print(heteronym_pairs)
-            pair = heteronym_pairs[i]
-            word_encoding[i] += vocal_encode_pinyin(pair[0], pair[1])
-
-    for i in range(counts):
-        ith = word_encoding[i]
-        current = copy.deepcopy(ith)
-        for character in word:
-            if encodings.get(character) is None:
-                # ying -> y
-                current = current + 'y'
+            # else we just append it to the encoding
+            if encountered_non_hanzi:
+                basic_encoding[-1][0][0] += initial[0].lower()
             else:
-                current = current + encodings[character][1][0][0]
-            print(current)
-            if MAX_LENGTH < len(current):
-                MAX_LENGTH += 1
-            word_encoding.append(copy.deepcopy(current))
+                encountered_non_hanzi = True
+                basic_encoding.append(([initial.lower()], ['']))
+            continue
+        else:
+            encountered_non_hanzi = False
 
-    
+        vocal_encoding = vocal_encode_pinyin(initial, final)
+        shape_encoding = encodings[ch][1]
+
+        basic_encoding.append(([vocal_encoding], shape_encoding))
+
+
+    # generate optional words selection pattern
+    # to best be with rime's selection method, we use the pattern of
+    # arbitrary full encoding of one of the characters
+    all_possible_encodings = [] # [[choices]]
+
+
+    word_encoding = []
+    for vocal, shape in basic_encoding:
+        all_possible_encodings.append(
+            list(map(lambda x: ''.join(x), itertools.product(vocal, shape))))
+
+    word_encoding = list(map(lambda x: ''.join(x), itertools.product(*all_possible_encodings)))
+    word_encoding.append(''.join(list(map(lambda x: ''.join(x[0]), basic_encoding))))
+
+    word_encoding = list(reversed(word_encoding))
+
+    print(word_encoding)
     print('---')
 
     return word_encoding
 
 
 def main():
-    global MAX_LENGTH
     if len(sys.argv) < 2:
         info_print('usage: {} db output')
         return
@@ -168,7 +177,9 @@ def main():
                 count += 1
                 info_print('{} / {}'.format(count, all_count))
 
-    info_print('max_length: {}'.format(MAX_LENGTH))
+        
+        max_length = list(c.execute('select max(length(word)) from dictionary'))[0][0]
+        info_print('max_length: {}'.format(max_length))
 
 
 if __name__ == '__main__':
